971d846c81ce66ff0789d5e8e415352944236bc4
YVOJBG <YVOJBG>
2026-02-22 11:37:50 +0100

    fix backend server env

--- server/app/main.py
+++ server/app/main.py
@@ -1,8 +1,18 @@
 from fastapi import FastAPI
+from fastapi.middleware.cors import CORSMiddleware
 
 from app.core.config import settings
 
 app = FastAPI(title=settings.APP_NAME)
+
+if settings.ENV == "dev":
+    app.add_middleware(
+        CORSMiddleware,
+        allow_origins=["http://localhost:5173", "http://127.0.0.1:5173"],
+        allow_credentials=True,
+        allow_methods=["*"],
+        allow_headers=["*"],
+    )
 
 
 @app.get("/")
